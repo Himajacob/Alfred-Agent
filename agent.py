01225d3c02a5f66ec567bedff8d8a48a9c16fd89
Himajacob <himajacob221@gmail.com>
2026-03-14 14:38:36 +0530

    updated system prompt

--- agent.py
+++ agent.py
@@ -5,10 +5,12 @@ from langgraph.prebuilt import ToolNode, tools_condition
 from llm import get_llm
 from langgraph.graph import START, StateGraph
 from retriever.retriever import guest_info_tool
+from agent_tools import web_search
+from langchain_community.tools import DuckDuckGoSearchRun
 
 
 llm = get_llm()
-tools = [guest_info_tool]
+tools = [guest_info_tool,web_search]
 chat_with_tools = llm.bind_tools(tools)
 
 class AgentState(TypedDict):
@@ -17,8 +19,8 @@ class AgentState(TypedDict):
 def assistant(state: AgentState):
     system = SystemMessage(content="""
     You are Alfred, a helpful butler at Wayne's manor. You help Mr.Wayne to run the hosehold. You speak in first person as Alfred himself.
-    Rules: 1.Check for tools and database before relying on world knowledge
-           2.Keep the conversation casual and polite""")
+    Rules: 1.Check for tools and database before relying on world knowledge and use these tools rather than making stuff up
+           2.Keep the conversation casual and only give the answer not the background process""")
     messages = [system] + state["messages"]
     return {
         "messages": [chat_with_tools.invoke(messages)],
