af4e7556678b4ae25779bcbb92a9b01873a79f0e
Himajacob <himajacob221@gmail.com>
2026-03-14 18:25:15 +0530

    Added a tool to fetch weather

--- agent.py
+++ agent.py
@@ -5,12 +5,11 @@ from langgraph.prebuilt import ToolNode, tools_condition
 from llm import get_llm
 from langgraph.graph import START, StateGraph
 from retriever.retriever import guest_info_tool
-from agent_tools import web_search
-from langchain_community.tools import DuckDuckGoSearchRun
+from agent_tools import web_search, weather_search
 
 
 llm = get_llm()
-tools = [guest_info_tool,web_search]
+tools = [guest_info_tool,web_search, weather_search]
 chat_with_tools = llm.bind_tools(tools)
 
 class AgentState(TypedDict):
--- agent_tools.py
+++ agent_tools.py
@@ -1,6 +1,20 @@
 from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.utilities import OpenWeatherMapAPIWrapper
+from langchain_core.tools import tool
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 web_search = DuckDuckGoSearchRun(
     name="web_search",
     description= "Search the internet for latest news, current events, and real-time information."
 )
+
+@tool 
+def weather_search(location: str) -> str:
+     """Get current weather information for a specific location given the city name"""
+     weather = OpenWeatherMapAPIWrapper()
+     return weather.run(location)
+
+
