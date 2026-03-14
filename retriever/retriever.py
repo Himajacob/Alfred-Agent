b6a299f917344114a8a78fc24be51efbfc641362
Himajacob <himajacob221@gmail.com>
2026-03-14 08:42:45 +0530

    changed the tool logic

--- retriever/retriever.py
+++ retriever/retriever.py
@@ -1,5 +1,5 @@
 from langchain_community.retrievers import BM25Retriever
-from langchain_core.tools import Tool
+from langchain_core.tools import Tool, tool
 
 bm25_retriever = None
 def initialize_retriever(docs):
@@ -7,16 +7,20 @@ def initialize_retriever(docs):
     if bm25_retriever is None:
         bm25_retriever = BM25Retriever.from_documents(docs)
 
-def extract_text(query: str)->str:
-    """Retrieves detailed information about gala guests based on their name or relation."""
+
+def guest_info_tool(query: str)->str:
+    """Retrieves detailed information about gala guests based on their name """
+
     results = bm25_retriever.invoke(query)
     if results:
         return "\n\n".join([doc.page_content for doc in results[:3]])
     else:
         return "No relevant information found about the guest."
 
-guest_info_tool = Tool(
-    name="guest_info_retriever",
-    func=extract_text,
-    description="Retrieves detailed information about gala guests based on their name or relation."
-)
+# guest_info_tool = Tool(
+#     name="guest_info_retriever",
+#     func=extract_text,
+#     description="Retrieves detailed information about gala guests based on their name or relation."
+# )
+
+
