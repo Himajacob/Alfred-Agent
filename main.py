84ae1afbb8081a217cde92194eba05a6116e3af0
Himajacob <himajacob221@gmail.com>
2026-04-06 19:56:32 +0530

    integrated with gradio

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessage
 from agent import build_graph
 from retriever.dataloaer import docs
 from retriever.retriever import initialize_retriever
-def main():
-    initialize_retriever(docs)
-    agent = build_graph()
-    messages = [
-        HumanMessage(
-            content="Tell me about our guest named 'Lady Ada Lovelace'"
-        )
-    ]
+import gradio as gr
 
+
+initialize_retriever(docs)
+agent = build_graph()
+
+def chat(message, history):
+    
+    # messages = [
+    #     HumanMessage(
+    #         content="can you give me the most downloaded model from hugging face hub by google "
+    #     )
+    # ]
+
+    messages = []
+
+    for user, bot in history:
+        messages.append(HumanMessage(content=user))
+        messages.append(AIMessage(content=bot))
+
+    messages.append(HumanMessage(content=message))
     response = agent.invoke({"messages": messages})
 
-    print("Alfred's Response:")
-    print(response["messages"][-1].content)
+    # print("Alfred's Response:")
+    # print(response["messages"][-1].content)
 
-if __name__ == "__main__":
-    main()
+    reply = response["messages"][-1].content
+    return reply
+
+gr.ChatInterface(
+    fn=chat,
+    title="🦇 Alfred - Wayne Manor Assistant",
+    description="Ask Alfred about guests, news, weather, or anything else."
+).launch()
