acd819c23c2a38bb1c14b20f37cd9571fc35ee49
Himajacob <himajacob221@gmail.com>
2026-04-09 18:53:26 +0530

    changed the model

--- llm.py
+++ llm.py
@@ -2,8 +2,10 @@ from langchain_ollama import ChatOllama
 
 def get_llm():
     return ChatOllama(
-        model="llama3.1:8b",
+        model="llama3.2:3b",
         temperature=0.7,
+        #base_url="http://host.docker.internal:11434",
     )
 #model="llama3.2:3b",
 #mistral:7b
+#model="llama3.1:8b"
